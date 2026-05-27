494de5fbaf62ed2137f031cd3f16b009dba8d71d
Anastasia <volvachmeow@gmail.com>
2026-05-27 16:30:00 +0300

    add app router with all page routes

---
+++ src/presentation/router/AppRouter.tsx
@@ -0,0 +1,65 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { ProtectedRoute } from '../components/common/ProtectedRoute';
+import { SiteLayout } from '../layouts/SiteLayout';
+import { AboutPage } from '../pages/AboutPage';
+import { AccountPage } from '../pages/AccountPage';
+import { AdminPage } from '../pages/AdminPage';
+import { CartPage } from '../pages/CartPage';
+import { ContactPage } from '../pages/ContactPage';
+import { HomePage } from '../pages/HomePage';
+import { LoginPage } from '../pages/LoginPage';
+import { NotFoundPage } from '../pages/NotFoundPage';
+import { PricingPage } from '../pages/PricingPage';
+import { RegisterPage } from '../pages/RegisterPage';
+import { TourDetailsPage } from '../pages/TourDetailsPage';
+import { ToursPage } from '../pages/ToursPage';
+import { WishlistPage } from '../pages/WishlistPage';
+
+export const AppRouter = () => (
+  <Routes>
+    <Route element={<SiteLayout />}>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/tours" element={<ToursPage />} />
+      <Route path="/tours/:slug" element={<TourDetailsPage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/pricing" element={<PricingPage />} />
+      <Route path="/contact" element={<ContactPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route
+        path="/wishlist"
+        element={
+          <ProtectedRoute role="user">
+            <WishlistPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/cart"
+        element={
+          <ProtectedRoute role="user">
+            <CartPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/account"
+        element={
+          <ProtectedRoute role="user">
+            <AccountPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute role="admin">
+            <AdminPage />
+          </ProtectedRoute>
+        }
+      />
+      <Route path="/404" element={<NotFoundPage />} />
+      <Route path="*" element={<Navigate to="/404" replace />} />
+    </Route>
+  </Routes>
+);
